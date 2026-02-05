713ce91cc3535050c4c6ace3af14eb8127027329
macbookpro <cdavidagoua@gmail.com>
2026-02-05 15:04:29 +0000

    update ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-16',
   runtimeConfig: {
     public: {
-      pocketbaseUrl: 'http://songon.space:8090', // ou ton URL distante
+      pocketbaseUrl: 'https://pb.songon.space', // ou ton URL distante
     },
   },
 })
